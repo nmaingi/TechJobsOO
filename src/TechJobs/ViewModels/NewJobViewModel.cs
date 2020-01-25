959b8ec65982ed3531cab1135e3cede669c7e774
Ngina Maingi <ngina_maingi@yahoo.com>
2020-01-25 17:13:59 -0600

    Completed TODO #4

--- src/TechJobs/ViewModels/NewJobViewModel.cs
+++ src/TechJobs/ViewModels/NewJobViewModel.cs
@@ -22,6 +22,7 @@ namespace TechJobs.ViewModels
         [Required]
         [Display(Name="Location")]
         public int LocationsID { get; set; }
+        
         public List<SelectListItem> Locations { get; set; } = new List<SelectListItem>();
         
         [Required]
@@ -41,14 +42,49 @@ namespace TechJobs.ViewModels
 
             foreach (Employer field in jobData.Employers.ToList())
             {
-                Employers.Add(new SelectListItem {
+                Employers.Add(new SelectListItem
+                {
                     Value = field.ID.ToString(),
                     Text = field.Value
                 });
             }
 
+            foreach (Location field in jobData.Locations.ToList())
+            {
+
+                Locations.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+            foreach (CoreCompetency field in jobData.CoreCompetencies.ToList())
+            {
+                CoreCompetencies.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+            foreach (PositionType field in jobData.PositionTypes.ToList())
+            {
+
+                PositionTypes.Add(new SelectListItem
+                {
+                    Value = field.ID.ToString(),
+                    Text = field.Value
+                });
+            }
+
+
             // TODO #4 - populate the other List<SelectListItem> 
             // collections needed in the view
+
+
+
+
+
+
 
         }
     }
